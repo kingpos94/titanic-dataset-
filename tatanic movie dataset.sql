2cd3ac1a5ddadabbae7f599c05cf7bfb56847936
adegunwa oluwaseyi <161047756+kingpos94@users.noreply.github.com>
2024-02-25 10:26:37 -0800

    Add files via upload

---
+++ tatanic movie dataset.sql
@@ -0,0 +1,82 @@
+ -- create database moviego; 
+--  create table moviego.moviego_list(
+--  id int not null,
+--  title varchar (20) not null,
+--  rating int not null,
+--  genre varchar (12) not null,
+--  released_date int not null);
+ 
+ -- insert into moviego.moviego_list values
+--  (150,"fast and furious",5,"action",2012),
+--  (100,"guns",4,"drama",2024),
+--  (619,"mission impossible",3,"thriller",2023),
+--  (50,"legend of the seeker",4,"sci-fi",2010),
+--  (30,"the lion king",3,"drama",1994),
+--  (200,"boy before flower",4,"romance",2013),
+--  (12,"vampire diaries",5,"horroe",2012),
+--  (120,"suits",3,"drama",2011);
+
+-- select * from moviego.moviego_list
+-- where genre="horror";
+
+
+-- SELECT AVG(rating) FROM moviego.moviego_list;
+
+-- select title from moviego.moviego_list
+-- where released_date =2015;
+
+-- select max(rating) from moviego.moviego_list;
+
+-- select count(title) from moviego.moviego_list
+-- where genre ="drama";
+
+-- select title,rating from moviego.moviego_list
+-- order by rating asc;
+
+-- select *, released_date from moviego.moviego_list
+-- order by rating
+
+-- select title, rating from moviego.moviego_list WHERE genre = 'Action'
+-- order by rating desc;
+
+-- select * from moviego.moviego_list
+-- where title like 'e%';
+
+-- select * from moviego.moviego_list;
+-- select count(title) as total_movies from moviego.moviego_list;
+
+
+-- Show movies with a title length between
+-- select * from moviego.moviego_list 
+-- where length(title) BETWEEN 10 AND 15;
+
+ -- Calculate the total number of movies in the table.
+
+-------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
+ 
+ 
+
+
+
